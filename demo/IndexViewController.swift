664047885813488e75964daf326ea7225b1b1638
sogreat <greatcup_ltw@hotmail.com>
2020-02-21 02:58:41 +0700

    update first second login

--- demo/IndexViewController.swift
+++ demo/IndexViewController.swift
@@ -15,40 +15,41 @@ class IndexViewController: UIViewController {
     
     let defaults = UserDefaults.standard
     
+    @IBOutlet weak var intro: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-         print("IndexViewController")
+        print("IndexViewController")
         // Do any additional setup after loading the view.
         if defaults.bool(forKey: "First Launch") == true {
-                   
-                   print("Second+")
-//                   button.sendActions(for: .touchUpInside)
-            // Run Code After First Launch
-                   
-                   defaults.set(true, forKey: "First Launch")
-                   
-               } else {
-                   
-                   
-                   print("First")
-                   
-                   // Run Code During First Launch
-                   defaults.set(true, forKey: "First Launch")
-                   
-               }
+            
+            print("Second+")
+//            intro.isHidden = true
+            defaults.set(true, forKey: "First Launch")
+            
+       
+            
+        } else {
+            
+            
+            print("First")
+            
+            // Run Code During First Launch
+            defaults.set(true, forKey: "First Launch")
+            
+        }
     }
-
+    
     @IBAction func indexButton(_ sender: Any) {
-          print("ButtonClicked")
+        print("ButtonClicked")
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
